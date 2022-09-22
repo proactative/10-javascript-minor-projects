781a3f25e74b81d048f8a270c9ced5aa99a0f5fb
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2022-09-22 16:27:17 +0300

    add: counter article

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 10-javascript-minor-projects
+
+https://proactative.github.io/10-javascript-minor-projects/
--- index.js
+++ index.js
@@ -3,42 +3,42 @@ const minutesItem = document.querySelector("#clock__minutes");
 const secondsItem = document.querySelector("#clock__seconds");
 
 
-function setCurrentTime()  {
-    //получаем данные с компьютера
-    let currentHours = new Date().getHours();
-    let currentMinutes = new Date().getMinutes();
-    let currentSeconds = new Date().getSeconds();
+function setCurrentTime() {
+  //получаем данные с компьютера
+  let currentHours = new Date().getHours();
+  let currentMinutes = new Date().getMinutes();
+  let currentSeconds = new Date().getSeconds();
 
-    hoursItem.innerText = currentHours;
-    minutesItem.innerText = currentMinutes;
-    secondsItem.innerText = currentSeconds;
+  hoursItem.innerText = currentHours;
+  minutesItem.innerText = currentMinutes;
+  secondsItem.innerText = currentSeconds;
 
-   //добавляем '0' в случае однозначного числа
-   if(currentHours < 10) {
+  //добавляем '0' в случае однозначного числа
+  if (currentHours < 10) {
     currentHours = '0' + currentHours;
-   } else {
+  } else {
     currentHours = currentHours;
-   }
+  }
 
-   if(currentMinutes < 10) {
+  if (currentMinutes < 10) {
     currentHours = '0' + currentMinutes;
-   } else {
+  } else {
     currentMinutes = currentMinutes;
-   }
-/*
-   if(currentSeconds < 10) {
-    currentSeconds = '0' + new Date().getSeconds();
-   } else {
-    currentSeconds = currentSeconds;
-   }*/
+  }
+  /*
+     if(currentSeconds < 10) {
+      currentSeconds = '0' + new Date().getSeconds();
+     } else {
+      currentSeconds = currentSeconds;
+     }*/
 
-   currentSeconds = currentSeconds < 10 ? '0' + currentSeconds : currentSeconds;
+  currentSeconds = currentSeconds < 10 ? '0' + currentSeconds : currentSeconds;
 
 
-    //обновляем каждую секунду
-    setTimeout(()=> {
-        setCurrentTime()
-    }, 1000)
+  //обновляем каждую секунду
+  setTimeout(() => {
+    setCurrentTime()
+  }, 1000)
 }
 
 setCurrentTime();
@@ -53,12 +53,52 @@ const cardElement = document.querySelector('.toggle');
 const titleElement = document.querySelector('.toggle__title');
 
 function updateBody() {
-    if(inputElement.checked) {
-        cardElement.style.backgroundColor = '#222';
-        titleElement.style.color = '#fff';
-    } else {
-        cardElement.style.backgroundColor = 'greenyellow';
-        titleElement.style.color = '#000';
-    }
+  if (inputElement.checked) {
+    cardElement.style.backgroundColor = '#222';
+    titleElement.style.color = '#fff';
+  } else {
+    cardElement.style.backgroundColor = 'greenyellow';
+    titleElement.style.color = '#000';
+  }
 }
 inputElement.addEventListener('input', updateBody);
+
+
+/*counter*/
+let count = 0;
+
+const box = document.querySelector('.counter__value-box');
+const counterValue = document.querySelector('.counter__value');
+const decrease = document.querySelector('.counter__button_type_minus');
+const increase = document.querySelector('.counter__button_type_plus');
+const reset = document.querySelector('.counter__button_type_reset');
+
+decrease.addEventListener('click', function() {
+  count--;
+  counterValue.textContent = count;
+  changeColor();
+})
+
+increase.addEventListener('click', function() {
+  count++;
+  counterValue.textContent = count;
+  changeColor();
+})
+
+reset.addEventListener('click', function() {
+  count = 0;
+  counterValue.textContent = count;
+  changeColor();
+})
+
+function changeColor() {
+ if(count > 0) {
+  box.style.backgroundColor = 'greenyellow';
+ } else if (count < 0) {
+  box.style.backgroundColor = '#e84118';
+ } else {
+  box.style.backgroundColor = '#fff';
+ }
+}
+
+
